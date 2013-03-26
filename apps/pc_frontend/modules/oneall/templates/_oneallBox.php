0605fb3bcd6e82b18aefd58267047ace79ad00b6
手嶋守 <tejima@gmail.com>
2013-03-26 23:21:55 +0900

    fix bug. 固定値だったのを設定ファイルから読み込む用に変更。

--- apps/pc_frontend/modules/oneall/templates/_oneallBox.php
+++ apps/pc_frontend/modules/oneall/templates/_oneallBox.php
@@ -1,6 +1,6 @@
 <script type="text/javascript">
  var oneall_js_protocol = (("https:" == document.location.protocol) ? "https" : "http");
- document.write(unescape("%3Cscript src='" + oneall_js_protocol + "://openpne.api.oneall.com/socialize/library.js' type='text/javascript'%3E%3C/script%3E"));
+ document.write(unescape("%3Cscript src='" + oneall_js_protocol + "://<?php echo opConfig::get('cqc.jp.oneall_subdomain'); ?>.api.oneall.com/socialize/library.js' type='text/javascript'%3E%3C/script%3E"));
 </script>
 
 <!-- The plugin will be embedded into this div //-->
