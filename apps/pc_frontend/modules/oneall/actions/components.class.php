e2d35968b822f081519f76a7757e3ddfa3335583
手嶋守 <tejima@gmail.com>
2013-03-26 23:10:09 +0900

    ログインフォームのガジェット化

---
+++ apps/pc_frontend/modules/oneall/actions/components.class.php
@@ -0,0 +1,11 @@
+<?php
+
+class oneallComponents extends sfComponents
+{
+
+ public function executeOneallBox()
+  {
+ //   $this->forms = $this->getUser()->getAuthForms();
+  }
+
+}
---
+++ apps/pc_frontend/modules/oneall/templates/_oneallBox.php
@@ -0,0 +1,16 @@
+<script type="text/javascript">
+ var oneall_js_protocol = (("https:" == document.location.protocol) ? "https" : "http");
+ document.write(unescape("%3Cscript src='" + oneall_js_protocol + "://openpne.api.oneall.com/socialize/library.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+
+<!-- The plugin will be embedded into this div //-->
+<div id="social_login_container"></div>
+
+<script type="text/javascript">
+ oneall.api.plugins.social_login.build("social_login_container", {
+  'providers' :  ['facebook', 'google', 'twitter'], 
+  'css_theme_uri': 'https://oneallcdn.com/css/api/socialize/themes/buildin/signin/large-v1.css',
+  'grid_size_x': '1',
+  'callback_uri': '<?php echo sfConfig::get('op_base_url'); ?>/member/login/authMode/Social'
+ });
+</script>
